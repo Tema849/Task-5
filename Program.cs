b0bd51495f8e403c43ee64669b6953484ba80dfd
Tema849 <qwaqwatern@gmail.com>
2023-04-02 13:26:14 +0300

    Задача 34

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// Пример:
+// [345, 897, 568, 234] -> 2.
+
+// Task:
+
+Console.WriteLine("Введите длинну массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandomNumbers(numbers);
+Console.WriteLine("В этом массиве: ");
+PrintArray(numbers);
+
+void RandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
+int count =0;
+for (int x = 0; x < numbers.Length; x++)
+{
+    if (numbers[x] % 2 == 0)
+    count++;
+}    
+Console.WriteLine($"из {numbers.Length} чисел, {count} чётных");
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+    
+
+// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// Пример:
+// [3, 7, 23, 12] -> 19.
+// [-4, -6, 89, 6] -> 0.
+
+// Task:
+
+
+
+// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// Пример:
+// [3 7 22 2 78] -> 76.
+
+// Task:
+
+
+
